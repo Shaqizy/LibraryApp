301d4f0190680bacca034e75fed0423a0e3313f0
Igor Frolov <shayzeq@gmail.com>
2022-05-12 14:37:43 +0300

    Ended all methods in AuthorController.kt

--- src/main/kotlin/com/shayzeq/libraryApp/controller/AuthorController.kt
+++ src/main/kotlin/com/shayzeq/libraryApp/controller/AuthorController.kt
@@ -7,9 +7,11 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -35,6 +37,18 @@ class AuthorController(val authorService: AuthorService) {
     fun createAuthor(@RequestBody authorDto: AuthorDto): ResponseEntity<Any> {
         val id = authorService.create(authorDto)
         return ResponseEntity(InfoMessageDto("Author with id = $id created successfully!"), HttpStatus.CREATED)
+    }
+    @PutMapping("/{id}")
+    @Operation(summary = "Обновелние автора по его идентификатору")
+    fun updateAuthor(@PathVariable id: String, @RequestBody authorDto: AuthorDto): ResponseEntity<Any> {
+        authorService.update(id, authorDto)
+        return ResponseEntity(InfoMessageDto("Author by id = $id updated!"), HttpStatus.OK)
+    }
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Удаление автора по его идентификатору")
+    fun deleteAuthor(@PathVariable id: String): ResponseEntity<Any> {
+        authorService.deleteById(id)
+        return ResponseEntity(InfoMessageDto("Deleted Author with id = $id successfully!"), HttpStatus.OK)
     }
 
 }
